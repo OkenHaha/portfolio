47323719a55936b7282feafe73d42b1d70bc68a6
OkenHaha <keithellakpamoken@gmail.com>
2024-08-18 18:05:04 +0530

    Initial Version

--- index.js
+++
@@ -1,9 +0,0 @@
-var nerd_slide = document.querySelector(".nerd_button");
-
-
-  nerd_slide.addEventListener("click", () => {
-    var tog = document.querySelector(".nerd");
-    tog.style.display = tog.style.display === "none" ? "block" : "none";
-  });
-
-console.log("hello");
